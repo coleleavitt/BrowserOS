92948487dd76539a3a70a5b241afeeca3274db0d
Nikhil Sonti <nikhilsv92@gmail.com>
2026-07-08 14:49:03 -0700

    Revert "feat: build resources"
    This reverts commit 44bdc24b9ac82664aff29ca93a1b1dd75243549b.

--- packages/browseros/chromium_patches/chrome/browser/browseros/server/resources/db/migrations/0003_scrub_hermes_credentials.sql
+++
@@ -1,9 +0,0 @@
-diff --git a/chrome/browser/browseros/server/resources/db/migrations/0003_scrub_hermes_credentials.sql b/chrome/browser/browseros/server/resources/db/migrations/0003_scrub_hermes_credentials.sql
-new file mode 100644
-index 0000000000000..956737aac4c4e
---- /dev/null
-+++ b/chrome/browser/browseros/server/resources/db/migrations/0003_scrub_hermes_credentials.sql
-@@ -0,0 +1,3 @@
-+UPDATE `agent_definitions`
-+SET `adapter_config_json` = NULL
-+WHERE `adapter` = 'hermes' AND `adapter_config_json` IS NOT NULL;
